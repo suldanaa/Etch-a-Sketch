ed08f5c513477d2f12a6643422861b1a4e6ac0d5
suldanaa <suldanaafrah@gmail.com>
2024-07-04 23:23:59 -0700

    Implemented ability to draw on the board

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 
 const boxes = document.getElementById('boxes');
+//controls all the boxes
 boxes.style.display = "flex";
 boxes.style.flexWrap = "wrap";
+boxes.style.position = "relative";
+boxes.style.marginLeft = "7px";
+boxes.style.height = "100%";
+boxes.style.width = "100%";
+
 
 
 for(let i=0; i<16; i++){
    for(let j=0; j<16; j++){
+   //controls individual boxes
     var box = document.createElement('div');
     box.className = "box";
-    box.style.border="1px solid grey";
+    box.style.border="1px solid lightgrey";
     box.style.flexBasis = "6%";
-    box.style.height = "5.1vh";
     boxes.appendChild(box);
    }
 }
 
+//determines all the boxes behavior
 let drawBox = document.getElementsByClassName("box");
-
-
+for(let i=0; i<drawBox.length; i++){
+   drawBox[i].addEventListener("mouseover", ()=>{
+      drawBox[i].style.backgroundColor = "grey";
+   });
+}
 
