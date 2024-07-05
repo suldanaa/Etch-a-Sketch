53ca8ac9f723a1cb43bff78c170b2415f542db5d
suldanaa <suldanaafrah@gmail.com>
2024-07-04 19:16:17 -0700

    Implemented basic 16x16 grid

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 
-const container = document.getElementById('container');
+const boxes = document.getElementById('boxes');
+boxes.style.display = "flex";
+boxes.style.flexWrap = "wrap";
+
+
+for(let i=0; i<16; i++){
+   for(let j=0; j<16; j++){
+    var box = document.createElement('div');
+    box.className = "box";
+    box.style.border="1px solid grey";
+    box.style.flexBasis = "6%";
+    box.style.height = "5.1vh";
+    boxes.appendChild(box);
+   }
+}
+
+let drawBox = document.getElementsByClassName("box");
+
+
 
