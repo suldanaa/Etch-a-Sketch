8e804dedcaa14570ce888b54d817a60128b0f15e
suldanaa <suldanaafrah@gmail.com>
2024-07-05 00:06:01 -0700

    Implemented user ability to change graph dimensions

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+let userDim=0;
+const btn = document.createElement('button');
+btn.textContent = "Change graph dimensions"
+btn.style.position = "Absolute";
+btn.style.fontSize = "24px";
+btn.style.marginLeft = "40%"
+btn.style.marginTop = ".5%";
 
-const boxes = document.getElementById('boxes');
+btn.addEventListener('click', ()=>{
+
+   userDim = prompt("Enter desired dimensions (1-100)");
+   while(userDim>100 || userDim<1){
+      userDim = prompt("Error: Invalid Dimension. Enter desired dimensions (1-100).");
+   }
+   const boxes = document.getElementById('boxes');
 //controls all the boxes
 boxes.style.display = "flex";
 boxes.style.flexWrap = "wrap";
@@ -8,10 +21,15 @@ boxes.style.marginLeft = "7px";
 boxes.style.height = "100%";
 boxes.style.width = "100%";
 
+//removes the graph if it's there
+if(boxes.hasChildNodes()){
+      while(boxes.firstChild){
+         boxes.removeChild(boxes.firstChild);
+      }
+}
 
-
-for(let i=0; i<16; i++){
-   for(let j=0; j<16; j++){
+for(let i=0; i<userDim; i++){
+   for(let j=0; j<userDim; j++){
    //controls individual boxes
     var box = document.createElement('div');
     box.className = "box";
@@ -29,3 +47,10 @@ for(let i=0; i<drawBox.length; i++){
    });
 }
 
+
+   
+});
+let clickEvent = new Event('click');
+btn.dispatchEvent(clickEvent);
+
+document.body.appendChild(btn);
